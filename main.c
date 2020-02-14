81ab1c605809af22e8a124322c016b862846493f
Justin Yang <yang9501@gmail.com>
2020-02-13 22:06:25 -0500

    Refactored code to include user defined functions

--- main.c
+++ main.c
@@ -13,21 +13,13 @@ int compareString(void const * a, void const * b) {
 	return strcmp(aa, bb);
 }
 
-int main() {
-	//1.  Retrieve Strings from user
-	//	A. Check for length
-	//	B. Check for duplicates
-	//	C. Check for illegal characters
-	char buffer[26];
-	char orderBuffer[2];
-	char storage[10][26];
-	char toCheck[8] = {'!', '@', '#', '$', '^', '(', ')','%'};
+void collectStrings(char storage[10][26], char buffer[26], char toCheck[8]) {
 	bool badCharExists, wrongLength, duplicateStringsExist;
 
 	printf("Enter unique ten character strings with a length between 1 and 25 characters.  The following characters are not allowed: !, @, #, $, %%, ^, (, ).\n");
 
 	for( int i = 0; i < 10; i++) {
-		printf("Enter string %d: ", i + 1);
+		printf("Enter string %d: \n", i + 1);
 
 		//Perform string checks for length, characters, and duplicate strings
 		wrongLength = true, badCharExists = true, duplicateStringsExist = true;
@@ -35,7 +27,7 @@ int main() {
 			fgets(buffer, 26, stdin);
 			//Checking length
 			if(strlen(buffer) < 2 || strlen(buffer) > 25 || strchr(buffer, '\n') == NULL) {
-				printf("Please enter a string between 1 and 25 characters: ");
+				printf("Please enter a string between 1 and 25 characters: \n");
 				wrongLength = true;
 				//Have to 'use' the rest of stdin so that it doesn't overflow to next fgets
 				if(strchr(buffer, '\n') == NULL) {
@@ -51,7 +43,7 @@ int main() {
 			int size = sizeof(toCheck)/sizeof(toCheck[0]);
 			for(int j = 0; j < size; j++) {
 				if(strchr(buffer, toCheck[j]) != NULL) {
-					printf("The characters !, @, #, $, %%, ^, (, ) are not allowed.  Please reenter a string.");
+					printf("The characters !, @, #, $, %%, ^, (, ) are not allowed.  Please reenter a string.\n");
 					badCharExists = true;
 					break;
 				}
@@ -64,7 +56,7 @@ int main() {
 			for (int k = 0; k < i; k++) {
 				if(strcmp(storage[k], buffer) == 0){
 					duplicateStringsExist = true;
-					printf("Please enter an unique string.");
+					printf("Please enter an unique string:\n");
 					break;
 				}
 				else {
@@ -84,28 +76,15 @@ int main() {
 		
 		//All conditions fulfilled, store the string 
 		strcpy(storage[i], buffer);
-		printf("Storage[%d]: %s, length: %lu\n", i, storage[i], strlen(storage[i]));
 	}
+	return;
+}
 
-	//2. Sort the strings based on ASCII value
-	printf("Pre-sorted strings are: \n");
-	for( int x = 0; x < 10; x++) {
-		printf("%d: %s\n", x + 1, storage[x]);
-	}
-
-	qsort(storage, 10, 26*sizeof(char), compareString);
-
-	printf("Sorted strings are: \n");
-	for( int x = 0; x < 10; x++) {
-		printf("%d: %s\n", x + 1, storage[x]);
-	}
-
-	//3. Print the sorted strings by value in ascending or descending value, depending on user input
+void printInOrder(char storage[10][26], char buffer[26], char orderBuffer[2]) {
 	bool improperInput = true;
 	while (improperInput) {		
 		printf("Choose a output order, (A)scending or (D)escending.  Please enter (A) or (D): \n");
 		fgets(orderBuffer, 2*sizeof(orderBuffer), stdin);
-		printf("Input length: %lu\n", strlen(orderBuffer));
 		if(strlen(orderBuffer) != 2 || strchr(buffer, '\n') == NULL) {
 			printf("Enter (A) or (D) for (A)scending or (D)escending.\n");
 			if(strchr(orderBuffer, '\n') == NULL) {
@@ -128,9 +107,35 @@ int main() {
 			improperInput = false;
 		}		
 	}
+	return;
+}
+
+void printHighAndLowValues(char storage[10][26]) {
+	printf("The string with the lowest ASCII value is %s\n", storage[0]);
+	printf("The string with the highest ASCII value is %s\n", storage[9]);
+}
+
+int main() {
+	//Initialize date storage
+	char buffer[26];
+	char orderBuffer[2];
+	char storage[10][26];
+	char toCheck[8] = {'!', '@', '#', '$', '^', '(', ')','%'};
+
+	//1.  Retrieve Strings from user
+	//	A. Check for length
+	//	B. Check for duplicates
+	//	C. Check for illegal characters
+	collectStrings(storage, buffer, toCheck);
+	
+	//2. Sort the strings based on ASCII value
+	qsort(storage, 10, 26*sizeof(char), compareString);
+
+	//3. Print the sorted strings by value in ascending or descending value, depending on user input
+	printInOrder(storage, buffer, orderBuffer);
 
 	//4. Print and label the string with the highest ASCII value and the lowest value.
-	printf("The string with the highest ASCII value is %s\n", storage[9]);
-	printf("The string with the lowest ASCII value is %s\n", storage[0]);
+	printHighAndLowValues(storage);
+
 	return 0;
 }
